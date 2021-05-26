12d4e4d8cfb6759f6e8e335fe069d99a05010105
xiaobai.li <admin>
2021-05-26 15:19:16 +0800

    更新代码

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-link to="/">home</router-link>|
+    <router-link to="/about">about</router-link>
+    <router-view />
   </div>
 </template>
 
